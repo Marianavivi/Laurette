b40f1c7303286e73b60124ecfa145a7a98893e0e
Mariana <marianagutierrezvivi@gmail.com>
2024-11-27 18:58:50 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// Configuration settings
+const config = {
+  authFile: './auth_info_multi.json', // File to store authentication state
+  botName: 'Laurette' // Name of your WhatsApp bot
+};
+
+module.exports = config;

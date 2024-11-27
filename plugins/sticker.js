58d9e85f90e76b8613c95ffd0f8eb88917e097fd
Mariana <marianagutierrezvivi@gmail.com>
2024-11-27 19:28:46 +0300

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -2,7 +2,6 @@ module.exports = {
   handleSticker: async (sock, msg) => {
     const from = msg.key.remoteJid;
 
-    // Respond to stickers with a thumbs up reaction
     if (msg.message && msg.message.stickerMessage) {
       await sock.sendMessage(from, { react: { text: '👍', key: msg.key } });
     }
